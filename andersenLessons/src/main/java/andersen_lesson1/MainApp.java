c88174d02ef17739dc7fb34fbea09ee51e80d741
Slavian94 <viacheslavbondar94@gmail.com>
2023-02-14 22:07:56 +0200

    firs home work lesson number 1

--- andersenLessons/src/main/java/andersen_lesson1/MainApp.java
+++ andersenLessons/src/main/java/andersen_lesson1/MainApp.java
@@ -1,4 +1,48 @@
 package andersen_lesson1;
 
 public class MainApp {
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange\n" +"Banana\n"+"Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5, b = 8;
+
+        if((a + b) >= 0) {
+            System.out.println("\nSum is positive");
+        } else {
+            System.out.println("\nSum is negative");
+        }
+    }
+
+    public static void printColor(){
+        int value = 50;
+
+        if (value <= 0) {
+            System.out.println("\nRED");
+        } else if (value > 0 || value < 100) {
+            System.out.println("\nYellow");
+        } else if (value > 100) {
+            System.out.println("\nGreen");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 62, b = 37;
+
+        if (a >= b) {
+            System.out.println("\na >= b");
+        } else {
+            System.out.println("\na < b");
+        }
+    }
 }
+
